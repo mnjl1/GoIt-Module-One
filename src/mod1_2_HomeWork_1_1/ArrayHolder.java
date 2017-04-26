bf6032f53802cf0cfc756c83cef7b6722f45cc4e
mnjl <d.manzhula@gmail.com>
2017-04-26 23:08:05 +0300

    Module 1.2, Home work 0102 - НЕ рабочая версия В ПАКЕТЕ !!!

---
+++ src/mod1_2_HomeWork_1_1/ArrayHolder.java
@@ -0,0 +1,80 @@
+package mod1_2_HomeWork_1_1;
+
+
+import java.util.*;
+
+public class ArrayHolder {
+    private int[] array;
+    int arraySize;
+    int numberToSearch;
+    Scanner scanner = new Scanner(System.in);
+    /*
+        public mod1_2_HomeWork_1_1.ArrayHolder(int size) {
+
+            this.array = new int[size];
+        }
+        */
+
+    public int[] getArray() {
+        return array;
+    }
+
+    public void setArray(int[] array) {
+        this.array = array;
+    }
+
+    public void fillArray(int arraySize) {
+
+            int count =0;
+
+            while(count < array.length)
+            try{ array[count] = Integer.parseInt(scanner.next());
+                count++;
+
+            }catch (Exception e) {
+                System.out.println("Not interger. Try again.");
+            }
+
+
+    }
+
+
+
+
+    // сортировка массива
+    public void sortArray() {
+        Arrays.sort(array);
+    }
+
+    //метод поиска числа по значению
+
+    public void searchArray() {
+        System.out.println("What number do you want to search?");
+
+        Scanner scanner = new Scanner(System.in);
+
+        boolean isInteger = false;
+
+        while (isInteger = false) {
+            try {
+
+                numberToSearch = Integer.parseInt(scanner.next());
+                isInteger = true;
+
+            } catch (Exception e) {
+                System.out.println("Not Integer, try again!");
+
+            }
+
+            for (int i = 0; i < arraySize; i++) {
+                System.out.println("Searching...");
+                if (numberToSearch == array[i]) {
+                    System.out.println("Number is found: " + numberToSearch + " array index: " + i);
+                } else System.out.println("Number is not found.");
+            }
+
+
+        }
+
+    }
+}
---
+++ src/mod1_2_HomeWork_1_1/ArrayTest.java
@@ -0,0 +1,12 @@
+package mod1_2_HomeWork_1_1;
+
+public class ArrayTest {
+    public static void main(String[] args) {
+
+
+        ConsoleHelper test = new ConsoleHelper();
+
+        test.menu();
+
+    }
+}
