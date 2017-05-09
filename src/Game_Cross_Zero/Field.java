a4c9d82688bf80a950b1e69f6fd2aca5ace26b0f
mnjl <d.manzhula@gmail.com>
2017-05-09 17:41:43 +0300

    Cross Zero ver 1.1

--- src/Game_Cross_Zero/Field.java
+++ src/Game_Cross_Zero/Field.java
@@ -1,7 +1,5 @@
 package Game_Cross_Zero;
 
-import java.util.Scanner;
-
 /**
  * Created by Dmytro on 08.05.2017.
  */
@@ -16,15 +14,6 @@ public class Field {
 
     }
 
-
-
-/*
-    public Field(int fieldSize) {
-        this.gameField = new  String[fieldSize][fieldSize];
-    }
-*/
-
-
     //illustrating a game field to see what squares are busy with"x" or "0"
     public void showGameField(String[][] gameField) {
         for (int i = 0; i < gameField.length; i++) {
@@ -33,31 +22,28 @@ public class Field {
 
             }
             System.out.println("\n");
-
-
         }
-
     }
 
     //checking if square is busy
     public void checkSquare(String[][] gameField) {
+        boolean legalMove = false;
         for (int i = 0; i <gameField.length ; i++) {
             for (int j = 0; j <gameField.length ; j++) {
-                if (gameField[i][j] == "x" || gameField[i][j] == "O") {
-                    System.out.println("Square is busy, make another choice.");
-                }
-
+                if (gameField[i][j] == "X" || gameField[i][j] == "O") {
+                    continue;
+                }else legalMove=true;
             }
-
         }
     }
+
 
     //checking squares for win
     public void checkWin(String[][] gameField) {
        if((gameField[0][0].equals("X") & gameField[0][1].equals("X") & gameField[0][2].equals("X")) ||
                (gameField[1][0].equals("X") & gameField[1][1].equals("X") & gameField[1][2].equals("X")) ||
                (gameField[2][0].equals("X") & gameField[2][1].equals("X") & gameField[2][2].equals("X")) ||
-               (gameField[0][0].equals("X") & gameField[1][1].equals("X") & gameField[1][2].equals("X")) ||
+               (gameField[0][0].equals("X") & gameField[1][1].equals("X") & gameField[2][2].equals("X")) ||
                (gameField[0][2].equals("X") & gameField[1][1].equals("X") & gameField[2][0].equals("X"))){
            System.out.println("Human wins!!");
            System.exit(0);
@@ -66,15 +52,12 @@ public class Field {
         else if((gameField[0][0].equals("O") & gameField[0][1].equals("O") & gameField[0][2].equals("O")) ||
                (gameField[1][0].equals("O") & gameField[1][1].equals("O") & gameField[1][2].equals("O")) ||
                (gameField[2][0].equals("O") & gameField[2][1].equals("O") & gameField[2][2].equals("O")) ||
-               (gameField[0][0].equals("O") & gameField[1][1].equals("O") & gameField[1][2].equals("O")) ||
+               (gameField[0][0].equals("O") & gameField[1][1].equals("O") & gameField[2][2].equals("O")) ||
                (gameField[0][2].equals("O") & gameField[1][1].equals("O") & gameField[2][0].equals("O"))){
            System.out.println("Robot wins!!");
            System.exit(0);
 
        }
 
-
     }
-
-
 }
--- src/Game_Cross_Zero/GameLogic.java
+++ src/Game_Cross_Zero/GameLogic.java
@@ -78,83 +78,107 @@ public class GameLogic {
                             break;
 
                         case (8):
-                            game.gameField[1][1] = "X";
+                            game.gameField[2][1] = "X";
                             break;
 
                         case (9):
-                            game.gameField[1][2] = "X";
+                            game.gameField[2][2] = "X";
                             break;
 
                         default:
-                            System.out.println("Make your choice, Human.");
-
-
-                    }continue;
-
-
-                }
-
-                //robot move logic
-                case (2):
-                    System.out.println("Robot is moving...");
-                    double robotChoice= Math.random()*10;
-                    int robotMove = (int) robotChoice;
-
-                    switch (robotMove) {
-
-                        case (1):
-                            game.gameField[0][0] = "O";
-
-                            break;
-
-                        case (2):
-                            game.gameField[0][1] = "o";
-                            break;
-
-                        case (3):
-                            game.gameField[0][2] = "O";
-                            break;
-
-                        case (4):
-                            game.gameField[1][0] = "O";
-                            break;
-
-                        case (5):
-                            game.gameField[1][1] = "O";
-                            break;
-
-                        case (6):
-                            game.gameField[1][2] = "O";
-                            break;
-
-                        case (7):
-                            game.gameField[2][0] = "O";
-                            break;
-
-                        case (8):
-                            game.gameField[1][1] = "O";
-                            break;
-
-                        case (9):
-                            game.gameField[1][2] = "O";
-                            break;
-
-                        default:
-                            System.out.println("Make your choice, Robot.");
-
-
-
+                            System.out.println("Make your choice, Human, planet Earth is in danger!!");
 
 
                     }
 
 
+                }break;
+
+                //robot move logic
+                case (2):
+
+                    int robotMove=0;
+                    boolean legalChoice= false;
+
+                    while (!legalChoice) {
+                        System.out.println("Robot is moving...");
+                        double robotChoice = Math.random() * 10;
+                        robotMove = (int) robotChoice;
+
+
+                    switch (robotMove) {
+
+                        case (1):
+                            if (game.gameField[0][0] == "X" || game.gameField[0][0] == "O") {
+                                continue;
+                            } else game.gameField[0][0] = "O";
+                            legalChoice = true;
+
+                            break;
+
+                        case (2):
+                            if (game.gameField[0][1] == "X" || game.gameField[0][1] == "O") {
+                                continue;
+                            } else game.gameField[0][1] = "o";
+                            legalChoice = true;
+                            break;
+
+                        case (3):
+                            if (game.gameField[0][2] == "X" || game.gameField[0][2] == "O") {
+                                continue;
+                            } else game.gameField[0][2] = "O";
+                            legalChoice = true;
+                            break;
+
+                        case (4):
+                            if (game.gameField[1][0] == "X" || game.gameField[1][0] == "O") {
+                                continue;
+                            } else game.gameField[1][0] = "O";
+                            legalChoice = true;
+                            break;
+
+                        case (5):
+                            if (game.gameField[1][1] == "X" || game.gameField[1][1] == "O") {
+                                continue;
+                            } else game.gameField[1][1] = "O";
+                            legalChoice = true;
+                            break;
+
+                        case (6):
+                            if (game.gameField[1][2] == "X" || game.gameField[1][2] == "O") {
+                                continue;
+                            } else game.gameField[1][2] = "O";
+                            legalChoice = true;
+                            break;
+
+                        case (7):
+                            if (game.gameField[2][0] == "X" || game.gameField[2][0] == "O") {
+                                continue;
+                            } else game.gameField[2][0] = "O";
+                            legalChoice = true;
+                            break;
+
+                        case (8):
+                            if (game.gameField[2][1] == "X" || game.gameField[1][1] == "O") {
+                                continue;
+                            } else game.gameField[2][1] = "O";
+                            legalChoice = true;
+                            break;
+
+                        case (9):
+                            if (game.gameField[2][2] == "X" || game.gameField[1][2] == "O") {
+                                continue;
+                            } else game.gameField[2][2] = "O";
+                            legalChoice = true;
+                            break;
+
+                        default:
+                            System.out.println("Make your choice, Robot.");
+
+                    }
+                }
             }
-
-
         }
-
-
     }
 }
 
