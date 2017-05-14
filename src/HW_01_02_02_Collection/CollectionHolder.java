6b7dc7cd3129f852ddfeb588b1dba3c9cb3c8b4a
mnjl <d.manzhula@gmail.com>
2017-05-14 15:43:54 +0300

    Collection testing. HW-01-02-02 ver 1.2

--- src/HW_01_02_02_Collection/CollectionHolder.java
+++ src/HW_01_02_02_Collection/CollectionHolder.java
@@ -6,18 +6,18 @@ import java.util.Scanner;
 /**
  * Created by Dmytro on 09.05.2017.
  */
-public class CollectionHolder {
+ public class CollectionHolder {
     public ArrayList<Integer> massive;
     int intNumber;
     int index;
 
-    public CollectionHolder() {
+    public   CollectionHolder() {
         massive = new ArrayList<>();
     }
      Scanner scanner = new Scanner(System.in);
 
 
-
+// method to enter any integer
     public void addInteger() {
         boolean isInteger = false;
         while (!isInteger) {
@@ -30,40 +30,103 @@ public class CollectionHolder {
     }
 
 
-
+//next element to collection
     public void addElement() {
-            System.out.println("Enter next collection element.");
+
+            System.out.println("Enter next element to collection.");
             addInteger();
             massive.add(intNumber);
+            int n; //increment, look at the task in ReadMe.txt
+        for (int i = 0; i <massive.size()-1; i++) {
+            if (massive.size()==0) break;
+            n = massive.get(i)+intNumber;
+            massive.set(i, n);
+        }
 
     }
 
-    public void setElement(int index, int intNumber) {
-        System.out.println("Enter index and new element to replace.");
-        addInteger();
-        index = intNumber;
-
-        System.out.println("Enter new number for replacing.");
-        addInteger();
-        massive.set(index, intNumber);
-    }
-
-    public void removeElement(int index) {
+    public void removeElement() {
         System.out.println("Enter index to remove element.");
+
+
+        int index=-1;
+        boolean correctIndex = false;
+        while(!correctIndex) {
+            addInteger();
+            if (intNumber < 0 || intNumber > massive.size()) {
+                System.out.println("Index is incorrect.");
+            } else {
+                index = intNumber;
+                correctIndex = true;
+            }
+        }
+        int delNumber = massive.get(index);
+        massive.remove(index);
+        int n;
+        for (int i = 0; i <massive.size(); i++) {
+            //if (massive.size() == 0) break;
+            n = massive.get(i) - delNumber;
+            massive.set(i, n);
+        }
+    }
+
+    //check if collection contains element
+    public void checkElement() {
+        System.out.println("Enter integer to check if collection contains it.");
+        addInteger();
+        boolean result = massive.contains(intNumber);
+
+       if (result) {
+           System.out.println("Yes");
+       }else System.out.println("No");
+
+
+    }
+
+    public void searchIndex() {
+        System.out.println("Enter index to look at element.");
         addInteger();
         index = intNumber;
-        massive.remove(index);
-
-
-
-    }
-
-    public void getElement(int index) {
-        System.out.println("Enter index to get element.");
-
-
         System.out.println(massive.get(index));
+    }
+
+    public void findMaxValue() {
+        int maxValue = 0;
+        for (Integer n: massive
+             ) {
+            if (n > maxValue) maxValue = n;
+
+        }
+        System.out.println("Max value is: " + maxValue);
+    }
+
+    public  void  findMinValue() {
+        int minValue = 0;
+        for (Integer n: massive
+             ) {
+            if (minValue < n) minValue = n;
+        }
+        System.out.println("Min value is: " +minValue);
+    }
+
+    public void calcAverage() {
+        int sum = 0;
+
+        for (Integer n: massive
+             ) {
+            sum+=n;
+
+        }
+        int average = sum/massive.size();
+        System.out.println("Average is: " + average);
 
     }
+
+    public void exitApp() {
+        System.out.println("Exiting...");
+        System.exit(0);
+
+    }
+
 
 }
--- src/HW_01_02_02_Collection/CollectionTest.java
+++ src/HW_01_02_02_Collection/CollectionTest.java
@@ -1,6 +1,6 @@
 package HW_01_02_02_Collection;
 
-import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  * Created by Dmytro on 09.05.2017.
@@ -9,10 +9,75 @@ public class CollectionTest {
     public static void main(String[] args) {
 
         CollectionHolder testCollection = new CollectionHolder();
-        testCollection.massive = new ArrayList<>();
+        while (true) {
+            System.out.println("Make your choice: ");
+            System.out.println("1.Add element to collection.");
+            System.out.println("2.Remove element from collection.");
+            System.out.println("3.Enter Integer to check if collection contains it.");
+            System.out.println("4.Enter Index to find element.");
+            System.out.println("5.Find Max value.");
+            System.out.println("6.Find Min value.");
+            System.out.println("7.Find Average value. ");
+            System.out.println("8.Exit app.");
+            System.out.println("9. Print collection."); //its not a task
 
-        testCollection.addElement();
-        testCollection.getElement(0);
+            Scanner scanner = new Scanner(System.in);
+            int choice = 0;
+            try{
+                 choice = scanner.nextInt();
+            }catch (Exception e) {
+                System.out.println("Not an integer!");
+            }
 
+
+            switch (choice) {
+
+                case (1): {
+                    testCollection.addElement();
+                    break;
+                }
+                case (2) : {
+                    testCollection.removeElement();
+                    break;
+                }
+                case (3) : {
+                    testCollection.checkElement();
+                    break;
+                }
+                case (4) : {
+                    testCollection.searchIndex();
+                    break;
+                }
+                case (5) : {
+                    testCollection.findMaxValue();
+                    break;
+                }
+                case (6) : {
+                    testCollection.findMinValue();
+                    break;
+                }
+                case (7) : {
+                    testCollection.calcAverage();
+                    break;
+                }
+                case (8) : {
+                    testCollection.exitApp();
+                }
+
+                case (9) : {
+                    for (Integer x : testCollection.massive
+                         ) {
+                        System.out.println(x);
+                    }
+                    break;
+                    
+                }
+                default:
+                    System.out.println(" Make correct choice.");
+
+            }
+
+
+        }
     }
 }
